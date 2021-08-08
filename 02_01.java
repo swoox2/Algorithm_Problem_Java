a9bfb4548287599c936fab1ab5cd18a9d32e2a13
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-08-09 00:15:10 +0900

    Update 02_01.java

--- 02_01.java
+++ 02_01.java
@@ -1,13 +1,11 @@
 import java.util.*;
 class Main02_01 {
-    public int[] solution(int n, int[] arr) {
-        int[] answer = new int[n];
-        answer[0]= arr[0];
-        for (int i = 0; i < arr.length-1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                answer[i + 1] = arr[i + 1];
-            }else{
-                i++;
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                answer.add(arr[i]);
             }
         }
         return answer;
@@ -21,7 +19,6 @@ class Main02_01 {
         for (int i = 0; i < num; i++) {
             arr[i]=scan.nextInt();
         }
-        /**System.out.println(T.solution(num,arr));**/
         for (int x : T.solution(num, arr)) {
             System.out.println(x + " ");
         }
