f99c924436aa64e22cdf5bb32c04c21498e02006
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-08-14 20:49:42 +0900

    Update 02_06.java

--- 02_06.java
+++ 02_06.java
@@ -3,12 +3,24 @@ class Main02_06 {
     public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-
+            int tmp = arr[i];
+            int reverse = 0;
+            while (!(tmp==0)) {
+                int t = tmp % 10;
+                tmp = tmp / 10;
+                reverse = reverse * 10 + t;
+            }
+            if(isPrime(reverse)) answer.add(reverse);
         }
-
         return answer;
     }
-
+    public boolean isPrime(int num) {  //소수인지 확인
+        if(num==1) return false;
+        for (int i = 2; i < num; i++) {
+            if(num%i==0) return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         Main02_06 T = new Main02_06();
         Scanner scan = new Scanner(System.in);
@@ -18,8 +30,7 @@ class Main02_06 {
             arr[i] = scan.nextInt();
         }
         for (int x : T.solution(n, arr)) {
-            System.out.print(x);
+            System.out.print(x+" ");
         }
     }
-
 }
