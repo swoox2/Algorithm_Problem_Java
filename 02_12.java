2bacd46c8a1e608bfa697db47c005ddc5709c86e
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-08-17 23:39:30 +0900

    Create 02_12.java

---
+++ 02_12.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Main02_12 {
+    public int solution(int n, int m, int[][] arr) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int cnt=0;
+                for (int k = 0; k < m; k++) {
+                    int pi=0,pj=0;
+                    for (int s = 0; s < n; s++) {
+                        if(arr[k][s]==i) pi=s;
+                        if(arr[k][s]==j) pi=s;
+                    }
+                    if(pi<pj) cnt++;
+                }
+                if(cnt==m) answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main02_12 T = new Main02_12();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int[][] arr = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = scan.nextInt();
+            }
+        }
+        System.out.println(T.solution(n,m,arr));
+    }
+}
