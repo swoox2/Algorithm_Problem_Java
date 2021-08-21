cdbc4771bca6b8ed754fd86f3b12ae3f14c21ee5
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-08-21 23:18:00 +0900

    Create 03_05.java

---
+++ 03_05.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Main03_05 {
+    public int solution(int n) {
+        int answer = 0, lt=1, sum=0;
+        for (int rt = 1; rt <= n / 2 + 1; rt++) {
+            sum+=rt;
+            if(sum==n) answer++;
+            while (sum > n) {
+                sum-=lt;
+                lt++;
+                if(sum==n) answer++;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main03_05 T = new Main03_05();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.println(T.solution(n));
+    }
+}
