44f3e2aae4b666c7ded01d5ce7cb41197b0647c6
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-08-22 20:58:42 +0900

    Update 03_05.java

--- 03_05.java
+++ 03_05.java
@@ -1,6 +1,16 @@
 import java.util.*;
 class Main03_05 {
     public int solution(int n) {
+    /** s1
+      int answer = 0, cnt = 1;
+        n--; // 두개이상의 연속된 자연수
+        while (n > 0) {
+            cnt++;
+            n = n - cnt;
+            if (n % cnt == 0) answer++;
+        }
+        return answer;
+    } **/
         int answer = 0, lt=1, sum=0;
         for (int rt = 1; rt <= n / 2 + 1; rt++) {
             sum+=rt;
