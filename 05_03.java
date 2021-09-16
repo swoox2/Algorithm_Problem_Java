76d7972a8b282e721c30368f5988b5c250c483c5
swoox2 <80804697+swoox2@users.noreply.github.com>
2021-09-16 13:13:26 +0900

    Create 05_03.java

---
+++ 05_03.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Main05_03 {
+    public int solution(int n, int[][] board, int m, int[] moves) {
+        int answer=0;
+        Stack<Integer> stack = new Stack<>();
+        for (int k = 0; k < m; k++) {
+                for (int j = 0; j < n; j++) {
+                    if (moves[k] == j + 1) {
+                        for (int i = 0; i < n; i++) {
+                            if (board[i][j] != 0) {
+                                if (!stack.isEmpty() && board[i][j] == stack.peek()) {
+                                    stack.pop();
+                                    answer += 2;
+                                } else {
+                                    stack.push(board[i][j]);
+                                    board[i][j]=0;
+                                    //break;
+                                }
+                                break;
+                            }
+                            //break;
+                        }
+                    }
+            }
+        }
+        //System.out.println(stack);
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main05_03 T = new Main05_03();
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[][] board = new int[n][n];
+        for (int i=0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = scan.nextInt();
+            }
+        }
+        int m = scan.nextInt();
+        int[] moves = new int[m];
+        for (int i = 0; i < m; i++) {
+            moves[i] = scan.nextInt();
+        }
+        System.out.println(T.solution(n,board,m,moves));
+    }
+}
