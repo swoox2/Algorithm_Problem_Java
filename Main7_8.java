04e28ca1ae10acc429d15c953aa585e5d79779dc
swoox2 <80804697+swoox2@users.noreply.github.com>
2023-06-29 21:15:26 +0900

    Add files via upload

---
+++ Main7_8.java
@@ -0,0 +1,39 @@
+package study;
+import java.util.*;
+public class Main7_8 {
+    int answer=0;
+    int[] dis = {1,-1,5};
+    int[] ch;
+    Queue<Integer> Q = new LinkedList<>();
+    public int BFS(int s, int e){
+        ch=new int[10001];
+        ch[s]=1;
+        Q.add(s);
+        int L=0;
+        while (!Q.isEmpty()){
+            int len=Q.size();
+            for(int i=0; i<len; i++){
+                int x=Q.poll();
+                if(x==e) return L;
+                for(int j=0; j<3; i++){
+                    int nx=x+dis[j];
+                    if(nx>=1 && nx<=10000 && ch[nx]==0){
+                        ch[nx]=1;
+                        Q.add(nx);
+                    }
+                }
+            }
+            L++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Main7_8 T = new Main7_8();
+        Scanner kb = new Scanner(System.in);
+        int s = kb.nextInt();
+        int e= kb.nextInt();
+        System.out.println(T.BFS(s,e));
+    }
+
+}
