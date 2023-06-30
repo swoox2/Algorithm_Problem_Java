9ad9a1de2655d34c2a3c96d3be3b3d734339a15a
swoox2 <80804697+swoox2@users.noreply.github.com>
2023-06-30 20:40:03 +0900

    Add files via upload

---
+++ Main7_11.java
@@ -0,0 +1,38 @@
+package study;
+import java.util.*;
+public class Main7_11 {
+    static int n, m, answer=0;
+    static int[][] graph;
+    static int[] ch;
+    public void DFS(int v){
+        //탈출조건
+        if(v==n) answer++;
+        // 재귀함수 수행동작
+        else{
+            for(int i=1; i<=n; i++){
+                if(ch[i]==0 && graph[v][i]==1){
+                    ch[i]=1;
+                    DFS(i);
+                    ch[i]=0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Main7_11 T = new Main7_11();
+        Scanner kb = new Scanner(System.in);
+        n=kb.nextInt();
+        m=kb.nextInt();
+        graph=new int[n+1][n+1];
+        ch=new int[n+1];
+        for(int i=0; i<m; i++){
+            int a=kb.nextInt();
+            int b=kb.nextInt();
+            graph[a][b]=1;
+        }
+        ch[1]=1;
+        T.DFS(1);
+        System.out.println(answer);
+    }
+}
