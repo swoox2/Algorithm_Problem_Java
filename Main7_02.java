f7d25bf5efaef413d2997a3d21f4a6f8300b02e7
swoox2 <80804697+swoox2@users.noreply.github.com>
2023-06-30 20:37:10 +0900

    Update Main7_02.java

--- Main7_02.java
+++ Main7_02.java
@@ -1,6 +1,6 @@
 package study;
 
-public class Main7_2 {
+public class Main7_02 {
     public void DFS(int n){
         if(n==0){
             return;
@@ -11,7 +11,7 @@ public class Main7_2 {
 
     }
     public static void main(String[] args){
-        Main7_2 T = new Main7_2();
+        Main7_02 T = new Main7_02();
         T.DFS(11);
     }
 }
