5d2aa9a08a9de01b5f510ba8a4c03a02e4fd0537
swoox2 <80804697+swoox2@users.noreply.github.com>
2023-07-06 18:52:53 +0900

    Update 04_03.java

--- 04_03.java
+++ 04_03.java
@@ -1,16 +1,19 @@
 import java.util.*;
 class Main04_03 {
-    public ArrayList<Integer> solution(int n, int k, int[] arr) {
+    public ArrayList<Integer> solution(int n, int k, int[] arr){
         ArrayList<Integer> answer = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < k-1; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<k-1; i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
-        int lt = 0;
-        for (int rt = k - 1; rt < n; rt++) {
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+
+        int lt=0, rt=k-1;
+        while(rt!=n){
+            map.put(arr[rt],map.getOrDefault(arr[rt],0)+1);
+            rt++;
             answer.add(map.size());
-            map.put(arr[lt], map.getOrDefault(arr[lt], 0) - 1);
+            map.put(arr[lt],map.get(arr[lt])-1);
             if(map.get(arr[lt])==0) map.remove(arr[lt]);
             lt++;
         }
