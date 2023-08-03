aded3d3fda8532e9c7ff96d59becaa82fc1ccaf4
Sang Woo <80804697+swoox2@users.noreply.github.com>
2023-08-03 22:41:20 +0900

    Add files via upload

---
+++ Main8_12.java
@@ -0,0 +1,11 @@
+package study;
+
+import java.util.*;
+
+public class Main8_12 {
+    public static void main(String[] args) {
+        Main8_12 T=new Main8_12();
+        Scanner scan=new Scanner(System.in);
+
+    }
+}
