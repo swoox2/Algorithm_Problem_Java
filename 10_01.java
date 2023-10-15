22143848a6e7eb296e8bc90d1db398cee1a45e3f
Sang Woo <80804697+swoox2@users.noreply.github.com>
2023-10-15 22:10:40 +0900

    Create 10_01.java

---
+++ 10_01.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    static int[] dp;
+    public static int solution(int n){
+        dp[1]=1;
+        dp[2]=2;
+
+        for(int i=3; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n=Integer.parseInt(br.readLine());
+        dp=new int[n+1];
+
+        bw.write(String.valueOf(solution(n)));
+
+
+        br.close();
+        bw.close();
+    }
+}
